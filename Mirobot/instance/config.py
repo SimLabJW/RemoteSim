f3db23db09d372fe9f56f7392c2295a6c1d379ba
SimLabJW <89232609+SimLabJW@users.noreply.github.com>
2024-07-27 03:00:17 +0900

    all file delete

--- Mirobot/instance/config.py
+++
@@ -1,3 +0,0 @@
-PORTNAME :str = "COM4" #--> 미로봇 시리얼 포트
-HOST :str = "Your IP ADDRESS" #--> Unity Device의 IP 주소
-PORT :int = "Your PORT" #--> Unity Device의 포트
--- instance/config.py
+++
@@ -1,1 +0,0 @@
-Model = ['Robomaster', 'TelloDrone', 'Mirobot']
